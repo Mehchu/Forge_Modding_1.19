630b8afa3cf2f347d86afe29a24f63c80fb3fab2
Mehchu <dtmackenzie06@outlook.com>
2022-06-26 11:58:39 +0100

    Added important comment

--- src/main/java/net/daniel/mehmod/MehMod.java
+++ src/main/java/net/daniel/mehmod/MehMod.java
@@ -14,8 +14,7 @@ import org.slf4j.Logger;
 @Mod(MehMod.MOD_ID)
 public class MehMod
 {
-
-
+    // Very Important Comment
     public static final String MOD_ID = "mehmod";
 
     private static final Logger LOGGER = LogUtils.getLogger();
